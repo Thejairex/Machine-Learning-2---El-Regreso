a8d184a89e021b8d9d1e0406a010e0cfa6108872
Thejairex <yairjesus777@gmail.com>
2024-06-26 21:14:42 -0300

    Iris Model Ends

--- Clasificador de iris/main.py
+++ Clasificador de iris/main.py
@@ -1,59 +1,52 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def linear_regression():
-    x = iris.drop(["Species"], axis=1) 
-    y = iris["Species"]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-    
-    lg = LogisticRegression()
-    lg.fit(x_train, y_train)
-    y_pred = lg.predict(x_test)
-    print('Precisión Regresión Logística: {}'.format(lg.score(x_train, y_train)))
-    # 0.9666666666666667
+class Iris:
+    def __init__(self):
+        iris = pd.read_csv("iris.csv")
+        iris.drop(["Id"], axis=1, inplace=True)
+        self.x = iris.drop(["Species"], axis=1) 
+        self.y = iris["Species"]
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x, self.y, test_size=0.2, random_state=0)
 
+        
+    def train_models(self, model):
+        model.fit(self.x_train, self.y_train)
+        
+        return model
+    
+    
+    def evaluate_model(self, model):
+        y_pred = model.predict(self.x_test)
+        print('Reporte de Clasificación:\n', classification_report(self.y_test, y_pred))
+        print("Matriz de Confusión", confusion_matrix(self.y_test, y_pred))
+        print('Precisión Entrenamiento: {}'.format(model.score(self.x_train, self.y_train)))
+        print('Precisión Testeo: {}'.format(model.score(self.x_test, self.y_test)))
+        print('Exactitud: ', accuracy_score(self.y_test, y_pred))
+        print("Validacion cruzada: ", cross_val_score(model, self.x, self.y, cv=4))
 
-def svm():
-    x = iris.drop(["Species"], axis=1)
-    y = iris["Species"]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+    def graf_maxtrix(self, matrix):
+        plt.figure(figsize=(10,7))
+        sns.heatmap(matrix, annot=True, fmt="d", cmap="Blues", xticklabels=self.y.unique(), yticklabels=self.y.unique())
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        plt.title('Matriz de Confusión')
+        plt.show()
     
-    svc = SVC()
-    svc.fit(x_train, y_train)
-    y_pred = svc.predict(x_test)
-    print('Precisión SVC: {}'.format(svc.score(x_train, y_train)))
-    #  0.9583333333333334
 
-def knn():
-    x = iris.drop(["Species"], axis=1)
-    y = iris["Species"]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-    
-    knn = KNeighborsClassifier()
-    knn.fit(x_train, y_train)
-    y_pred = knn.predict(x_test)
-    print('Precisión KNN: {}'.format(knn.score(x_train, y_train)))
-    # 0.95
-    
-def decision_tree():
-    x = iris.drop(["Species"], axis=1)
-    y = iris["Species"]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
-    
-    arbol = DecisionTreeClassifier()
-    arbol.fit(x_train, y_train)
-    y_pred = arbol.predict(x_test)
-    print('Precisión Decision Tree: {}'.format(arbol.score(x_train, y_train)))
-    # 1.0
-    
 if __name__ == "__main__":
-    iris = pd.read_csv("iris.csv")
-    iris.drop(["Id"], axis=1, inplace=True)
+    iris = Iris()
+    models = [LogisticRegression(), SVC(), KNeighborsClassifier(), DecisionTreeClassifier()]
     
-    decision_tree()
-
+    for model in models:
+        model = iris.train_models(model)
+        iris.evaluate_model(model)
+        print("-" * 40)
